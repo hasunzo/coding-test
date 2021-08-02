c7e7aeaabc30af1132cd91abd0494da407895330
hasunzo <gktjsdl470@gmail.com>
2021-08-02 14:52:22 +0900

    Level1 위클리 챌린지 1주차

---
+++ coding-test/level1/Programmers82612.java
@@ -0,0 +1,25 @@
+package level1;
+
+/**
+ *
+ * 위클리 챌린지 1주차
+ *
+ */
+public class Programmers82612 {
+    public static void main(String[] args) {
+        Programmers82612 programmers82612 = new Programmers82612();
+        long answer = programmers82612.solution(3, 20, 4);
+        System.out.println(answer);
+
+    }
+    public long solution(int price, int money, int count) {
+        long answer = price + (price*count);
+        answer = (answer * count)/2 - money ;
+
+        if(answer > 0) {
+            return answer;
+        } else {
+            return 0;
+        }
+    }
+}
