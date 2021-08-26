b1433627be011d951322e0713045c9083111852a
hasunzo <gktjsdl470@gmail.com>
2021-08-27 00:26:47 +0900

    Level1 문자열 내 마음대로 정렬하기

--- coding-test/level1/Programmers12915.java
+++ coding-test/level1/Programmers12915.java
@@ -10,23 +10,28 @@ import java.util.*;
 public class Programmers12915 {
     public static void main(String[] args) {
         Programmers12915 programmers12915 = new Programmers12915();
-        programmers12915.solution(new String[] {"sun", "bed", "car"}, 1);
+        programmers12915.solution(new String[] {"abcd", "abce", "cdx"}, 2);
     }
     public String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
 
         Arrays.sort(strings);
-        Map<Integer, String> map = new HashMap<>();
+
+        Map<Integer, Integer> map = new HashMap<>();
+
         for(int i = 0; i < strings.length; i++) {
-            map.put((int) strings[i].charAt(n), strings[i]);
+
+            map.put(i, (int)strings[i].charAt(n));
         }
 
-        Object[] mapKey = map.keySet().toArray();
-        Arrays.sort(mapKey);
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+
+        Collections.sort(keySet, (o1, o2) -> map.get(o1).compareTo(map.get(o2)));
 
         int index = 0;
-        for(Integer keyVal : map.keySet()) {
-            answer[index] = map.get(keyVal);
+        for(Integer key : keySet) {
+
+            answer[index++] = strings[key];
         }
 
         return answer;
